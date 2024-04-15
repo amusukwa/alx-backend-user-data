54f38c4c1bdeaf9c8ca213159797ae8113594a18
alice <amusukwa@gmail.com>
2024-04-15 20:21:10 +0200

    func require

--- 0x01-Basic_authentication/ec2f874b061bd3a2915949f081f4f5f055104f20/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/ec2f874b061bd3a2915949f081f4f5f055104f20/SimpleAPI/api/v1/auth/auth.py
@@ -3,7 +3,16 @@ from typing import List, TypeVar
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        path = path.rstrip('/')
+        for excluded_path in excluded_paths:
+            excluded_path = excluded_path.rstrip('/')
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         return None
