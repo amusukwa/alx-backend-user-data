5f5c1b8eece08808bcd5ac069dd6e7c7c725ab40
alice <amusukwa@gmail.com>
2024-04-16 14:45:20 +0200

    users object

--- 0x01-Basic_authentication/ec2f874b061bd3a2915949f081f4f5f055104f20/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/ec2f874b061bd3a2915949f081f4f5f055104f20/SimpleAPI/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ module for base64 decoding"""
 import base64
+from models.user import User
 
 
 class BasicAuth:
@@ -27,3 +28,13 @@ class BasicAuth:
             return None, None
         user_email, user_password = decoded_base64_authorization_header.split(':', 1)
         return user_email, user_password
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> User:
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({"email": user_email})
+        if not users:
+            return None
