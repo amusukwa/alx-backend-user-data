67dcb1314214ecaca8f24588923091b2e5b0c40f
alice <amusukwa@gmail.com>
2024-04-16 09:07:29 +0200

    basic_auth extract_user_cred

--- 0x01-Basic_authentication/ec2f874b061bd3a2915949f081f4f5f055104f20/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/ec2f874b061bd3a2915949f081f4f5f055104f20/SimpleAPI/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+""" module for base64 decoding"""
 import base64
+
 
 class BasicAuth:
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
@@ -7,4 +9,24 @@ class BasicAuth:
             return None
         else:
             return authorization_header.split(" ")[1]
+
+    
+     def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except:
+            return None
+
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        if decoded_base64_authorization_header is None or not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        user_email, user_password = decoded_base64_authorization_header.split(':', 1)
+        return user_email, user_password        
 
