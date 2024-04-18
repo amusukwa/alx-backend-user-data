f0a016720aff4b633e6437b39f5f143238b8b58d
alice <amusukwa@gmail.com>
2024-04-18 19:24:33 +0200

    indentation error

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,8 +21,8 @@ class SessionAuth(Auth):
             return None
         return self.user_id_by_session_id.get(session_id)
 
-     def current_user(self, request=None) -> User:
-         """ gets current user from session"""
+    def current_user(self, request=None) -> User:
+        """ gets current user from session"""
         if request is None:
             return None
         session_id = self.session_cookie(request)
