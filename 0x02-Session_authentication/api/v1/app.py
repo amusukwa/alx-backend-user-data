e6cada63445aafc5cee211e492f22dfa69b73875
alice <amusukwa@gmail.com>
2024-04-18 13:13:10 +0200

    task 1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.session_auth import SessionAuth
 import os
 
 
@@ -16,13 +17,13 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-auth_type = os.environ.get('AUTH_TYPE')
-if auth_type == 'basic_auth':
+auth_type = getenv('AUTH_TYPE')
+if auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+else:
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-else:
-    from api.v1.auth.auth import Auth
-    auth = Auth()
 
 @app.before_request
 def before_request():
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module for class SessionAuth"""
+
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    pass
