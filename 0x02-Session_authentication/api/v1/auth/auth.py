7e41b85f942131426a1f73d7498d8f0006a0449f
root <amusukwa@gmail.com>
2024-04-21 13:41:36 +0000

    documentation

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -7,6 +7,7 @@ from flask import Request
 
 class Auth:
     """ Class Auth for authorization"""
+    
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ function for getting authorization"""
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
@@ -22,6 +23,7 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ function for getting auth header"""
+        
         if request is None or 'Authorization' not in request.headers:
             return None
         return request.headers.get('Authorization')
@@ -29,10 +31,12 @@ class Auth:
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ function for current user"""
+        
         return None
 
     def session_cookie(self, request: Request = None) -> str:
         """ returns session cookie"""
+        
         if request is None:
             return None
         session_name = os.environ.get('SESSION_NAME', '_my_session_id')
