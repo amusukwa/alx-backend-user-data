4ce3f21c08df763d44982e3337635f257d1f8ec2
root <amusukwa@gmail.com>
2024-04-23 20:59:24 +0000

    task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -28,5 +28,38 @@ def register() -> str:
         # User already registered
         return jsonify({"message": "email already registered"}), 400
 
+@app.route("/sessions", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        abort(400)
+
+    if not auth.valid_login(email, password):
+        abort(401)
+
+    session_id = auth.create_session(email)
+    if not session_id:
+        abort(500)
+
+    response = make_response(jsonify({"email": email, "message": "logged in"}), 200)
+    response.set_cookie("session_id", session_id)
+    return response
+    
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    session_id = request.cookies.get("session_id")
+    if not session_id:
+        return jsonify({"error": "Session ID not provided"}), 400
+
+    user = auth.get_user_from_session_id(session_id)
+    if user:
+        auth.destroy_session(user.id)
+        return redirect("/")
+    else:
+        return jsonify({"error": "User not found"}), 403
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,10 +3,10 @@
 """
 
 import bcrypt
+import uuid
+from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
-from sqlalchemy.orm.exc import NoResultFound
-import uuid
 
 
 class Auth:
@@ -53,3 +53,19 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Returns the user corresponding to the given session ID, or None if not found."""
+        if not session_id:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Updates the session ID of the user with the given user_id to None."""
+        self._db.update_user(user_id, session_id=None)
+        
