ee0b92ff209435ee561a3e35ff7d4f2f65e93fe7
root <amusukwa@gmail.com>
2024-04-23 14:57:45 +0000

    app.py

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""app.py - Flask app with a single GET route"""
+
+from flask import Flask, jsonify
+from auth import Auth
+
+app = Flask(__name__)
+auth = Auth()
+
+
+@app.route("/")
+def welcome() -> str:
+    """Welcome route returning a JSON payload"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
