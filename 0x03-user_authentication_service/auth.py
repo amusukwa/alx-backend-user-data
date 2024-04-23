6a2bfacab4f8ea948642d05299434a6bc75e066a
root <amusukwa@gmail.com>
2024-04-23 11:49:33 +0000

    task 6

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,17 +1,51 @@
 #!/usr/bin/env python3
-"""auth module
+"""auth.py
 """
 
-import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
-def _hash_password(password: str) -> bytes:
-    """Hashes the input password string using bcrypt.hashpw
+
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode(), salt)
-    return hashed_password
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """Hashes the input password string using bcrypt.hashpw
+        """
+        # Implement _hash_password method here (as defined in the previous task)
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user with the provided email and password
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
 
 # Usage example
 if __name__ == "__main__":
-    print(_hash_password("Hello Holberton"))
+    email = 'me@me.com'
+    password = 'mySecuredPwd'
+
+    auth = Auth()
+
+    try:
+        user = auth.register_user(email, password)
+        print("Successfully created a new user!")
+    except ValueError as err:
+        print("Could not create a new user: {}".format(err))
+
+    try:
+        user = auth.register_user(email, password)
+        print("Successfully created a new user!")
+    except ValueError as err:
+        print("Could not create a new user: {}".format(err))
 
