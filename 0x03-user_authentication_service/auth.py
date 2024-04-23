8b84265de736a4ae063afccb022b64cc37c8c100
root <amusukwa@gmail.com>
2024-04-23 18:42:25 +0000

    task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 class Auth:
@@ -38,3 +39,17 @@ class Auth:
             return bcrypt.checkpw(password.encode(), user.hashed_password)
         except NoResultFound:
             return False
+
+    def _generate_uuid(self) -> str:
+        """Generates a new UUID and returns its string representation."""
+        return str(uuid.uuid4())
+
+    def create_session(self, email: str) -> str:
+        """Creates a session for the user with the given email."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = self._generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
