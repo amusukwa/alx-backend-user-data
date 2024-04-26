35929e7a557041117c48eea301682e1b47c12712
root <amusukwa@gmail.com>
2024-04-26 11:06:58 +0000

    auth error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-"""auth.py
-"""
+"""auth.py"""
 
 import bcrypt
 import uuid
 from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes the input password string using bcrypt.hashpw."""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
 
 
 class Auth:
@@ -16,19 +22,13 @@ class Auth:
         """Constructor function"""
         self._db = DB()
 
-    def _hash_password(self, password: str) -> bytes:
-        """Hashes the input password string using bcrypt.hashpw."""
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode(), salt)
-        return hashed_password
-
     def register_user(self, email: str, password: str) -> User:
         """Registers a new user with the provided email and password."""
         try:
             existing_user = self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
 
@@ -85,7 +85,7 @@ class Auth:
         """Updates the password for the user with the given reset token."""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             update = self._db.update_user()
             update(user.id, hashed_password=hashed_password, reset_token=None)
         except NoResultFound:
